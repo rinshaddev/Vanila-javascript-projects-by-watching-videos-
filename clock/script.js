88bf82eb291d110695ba5c6952cc4affed652631
rinshaddev <rinshadr843@gmail.com>
2023-03-09 14:18:48 +0530

    Add logic to rotate the clock

---
+++ clock/script.js
@@ -0,0 +1,25 @@
+
+
+const secondHand = document.getElementById('second');
+const minuteHand = document.getElementById('minute');
+const hourHand = document.getElementById('hour');
+
+window.onload = setInterval(() => {
+    setClock();
+}, 1000);
+
+function setClock(){
+    const currentDate = new Date()
+    const secondsRatio = currentDate.getSeconds() / 60
+    const minuteRatio = (secondsRatio + currentDate.getMinutes() )/ 60
+    const hourRatio = (minuteRatio + currentDate.getHours()) /12 
+    setRotation(secondHand, secondsRatio)
+    setRotation(minuteHand, minuteRatio)
+    setRotation(hourHand, hourRatio)
+    
+    
+}
+
+function setRotation(element , rotationRatio) {
+    element.style.transform=`rotate(${rotationRatio * 360}deg)`
+}
